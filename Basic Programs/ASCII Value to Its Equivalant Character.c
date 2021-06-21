c6246a39cf49d0e27b733a768864637750dd96d4
Aditya9854 <85945553+Aditya9854@users.noreply.github.com>
2021-06-21 13:47:09 +0530

    Add files via upload

---
+++ Basic Programs/ASCII Value to Its Equivalant Character.c
@@ -0,0 +1,18 @@
+
+   #include<stdio.h>
+   #include<conio.h>
+
+   int main()
+   {
+            int val = 0;
+
+            printf("\n\nEnter an Ascii Value to Get Corrosponding Character => ");
+            scanf("%d",&val);
+
+            printf("\n Character for Given Ascii Value= <%c> \n",val);
+
+            getch();
+            return(0);
+
+   }
+
